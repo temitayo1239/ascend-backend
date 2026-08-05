f130351b6d43b4ed9d5220fecd04f7e6e811061e
temitayo1239 <teefresh1239@gmail.com>
2026-08-05 15:40:46 +0100

    chore: setup backend foundation

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Ascend API')
+    .setDescription('Ascend Backend API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 5000);
+
+  console.log(
+    `🚀 Server running at http://localhost:${process.env.PORT || 5000}`,
+  );
+}
+
+bootstrap();
